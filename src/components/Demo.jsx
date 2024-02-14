81dc6a9bff44c4f51513315400b748826b47346c
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-14 00:41:05 -0600

    view erticles /

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+"use client";
 
-export default function Demo() {
+import React, { useState, useEffect } from "react";
+import { useLazyGetSummaryQuery } from "@/lib/article.js";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const paragraphs = article.summary
+    .split(/<\/?p>/)
+    .filter((paragraph) => paragraph.trim() !== "");
+
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
+
+  // RTK lazy query
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  // Load data from localStorage on mount
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+
+    if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      // update state and local storage
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
+  // copy the url and toggle the icon for user feedback
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
       <div className="flex flex-col w-full gap-2">
-        <form className="relative lex justify-center items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="relative lex justify-center items-center"
+        >
           <img
             src="/assets/link.svg"
             alt="Link Icon"
@@ -12,27 +79,114 @@ export default function Demo() {
           />
           <input
             type="url"
-            placeholder="Ingresa la URL"
-            value=""
+            placeholder="Ingrese la URL"
+            value={article.url}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
+            onKeyDown={handleKeyDown}
             required
             className="block w-full rounded-md border border-gray-200 bg-white py-2.5 pl-10 pr-12 text-sm shadow-lg font-satoshi font-medium focus:border-black focus:outline-none focus:ring-0 peer"
           />
+          <div>
+            
           <button
             type="submit"
-            className="hover:border-gray-700 hover:text-gray-700 absolute inset-y-0 right-0 my-1.5 mx-1.5 flex w-10 items-center justify-center rounded border border-gray-200 font-sans text-sm font-medium text-gray-400 peer-focus:border-gray-700 peer-focus:text-gray-700 "
+            className="hover:border-gray-700 hover:text-gray-700 absolute inset-y-0 right-0 my-1.5 mr-1.5 flex w-10 items-center justify-center rounded border border-gray-200 font-sans text-sm font-medium text-gray-400  peer-focus:border-gray-700 peer-focus:text-gray-700 "
           >
-            OK
+            <p>OK</p>
           </button>
-        </form>
-        {/* Historial */}
-        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           
+          </div>
+        </form>
+
+        {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          <div>
+          <h2 className="font-satoshi font-bold text-gray-600 text-xl pb-5">
+                <span className="font-black bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent">
+                  Historial:
+                </span>
+              </h2>
+          </div>
+          {allArticles.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="p-3 flex justify-start items-center flex-row bg-white border border-gray-200 gap-3 rounded-lg cursor-pointer"
+            >
+              <div
+                className="w-7 h-7 rounded-full bg-white/10 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur flex justify-center items-center cursor-pointer"
+                onClick={() => handleCopy(item.url)}
+              >
+                <img
+                  src={
+                    copied === item.url
+                      ? "/assets/tick.svg"
+                      : "/assets/copy.svg"
+                  }
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
         </div>
+        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
       </div>
-      {/* Resultado de Búsqueda */}
+
+      {/* Display Results */}
       <div className="my-10 max-w-full flex justify-center items-center">
-        
+        {isFetching ? (
+          <img
+            src="/assets/loader.svg"
+            alt="loader"
+            className="w-20 h-20 object-contain"
+          />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Oh!, Esto no debe de estar pasando, intenta con otra url más
+            específica.
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl pb-5">
+                Artículo{" "}
+                <span className="font-black bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent">
+                  Resumido:
+                </span>
+              </h2>
+              <div className="rounded-xl border border-gray-200 bg-white/20 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur p-4">
+                <div className="rounded-xl border border-gray-200 bg-white/20 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur p-4">
+                  <div className="rounded-xl border border-gray-200 bg-white/20 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur p-4">
+                    {paragraphs.map((paragraph, index) => (
+                      <p
+                        key={index}
+                        className="font-inter font-medium text-sm text-gray-700 text-justify"
+                      >
+                        {paragraph}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
-}
+};
+
+export default Demo;
